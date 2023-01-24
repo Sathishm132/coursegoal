b516e5e87356f11878c448d4e25dcd16a8e51006
Your Name <you@example.com>
2023-01-24 21:22:51 +0530

    CourseGoals

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const[isvalid, setIsvalid]=useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length>0){
+      setIsvalid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0){
+      setIsvalid(false)
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isvalid?"invalid":''}`}>
+        <label >Course Goal</label>
+        <input  type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button validity={isvalid} type="submit">Add Goal</Button>
     </form>
   );
 };
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,8 +3,9 @@ import React from 'react';
 import './Button.css';
 
 const Button = props => {
+  console.log(props)
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={`button ${!props.validity?'invalid':''}`} onClick={props.onClick}>
       {props.children}
     </button>
   );
